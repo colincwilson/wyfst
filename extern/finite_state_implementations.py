c120415d95dc618d17d2bd188cb083db3bdac697
Colin Wilson <colin.wilson@jhu.edu>
2026-06-17 22:43:29 -0400

    other finite-state implementations

---
+++ extern/finite_state_implementations.py
@@ -0,0 +1,14 @@
+OpenFst
+https://www.openfst.org/twiki/bin/view/FST/WebHome
+
+Pynini
+https://www.opengrm.org/twiki/bin/view/GRM/Pynini
+
+Foma
+https://fomafst.github.io/
+
+pyfoma
+https://mhulden.github.io/pyfoma/
+
+pyformlang
+https://pyformlang.readthedocs.io/en/latest/
